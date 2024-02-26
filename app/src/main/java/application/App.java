21f3a33a7c12b91a637ec4c57652eb657afe7960
thiago BDAg <thiago.souza99@fatec.sp.gov.br>
2024-02-26 17:44:18 +0000

    Innit App

---
+++ app/src/main/java/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }    
+}
